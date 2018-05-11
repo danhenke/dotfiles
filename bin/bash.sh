3ed86ea396ac70b5aa9de5bbbb0ddcc1409b5d2b
Dan Henke <dan@thinkalike.com>
2018-05-11 21:02:37 +1000

    Adding git=hub alias

--- bin/bash.sh
+++ bin/bash.sh
@@ -20,6 +20,9 @@ export GREP_OPTIONS='--color=auto'
 # Pretty prompt
 export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\] \$ "
 
+# Aliases
+alias git=hub
+
 # Add colour to man
 man() {
 	env \
