3747b948ed471e2d73d43821d774fb82a80843bf
Dan Henke <dan@thinkalike.com>
2018-08-13 16:21:39 +1000

    Setting pager to diff-highlight

--- bin/git.sh
+++ bin/git.sh
@@ -5,3 +5,6 @@ git config user.email "dan@thinkalike.com"
 git config user.name "Dan Henke"
 git config push.default simple
 git config --global credential.helper osxkeychain
+git config --global pager.log `brew --prefix`/share/git-core/contrib/diff-highlight/diff-highlight
+git config --global pager.show `brew --prefix`/share/git-core/contrib/diff-highlight/diff-highlight
+git config --global pager.diff `brew --prefix`/share/git-core/contrib/diff-highlight/diff-highlight
