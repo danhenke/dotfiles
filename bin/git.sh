a6e0f8cc8c227d2bfc61d04ed285c1423a625f0e
Dan Henke <dan@thinkalike.com>
2019-10-10 01:27:39 +1000

    Added global to user config

--- bin/git.sh
+++ bin/git.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-git config user.email "dan@thinkalike.com"
-git config user.name "Dan Henke"
-git config push.default simple
+git config --global user.email "dan@thinkalike.com"
+git config --global user.name "Dan Henke"
 git config --global credential.helper osxkeychain
 git config --global pager.log `brew --prefix`/share/git-core/contrib/diff-highlight/diff-highlight
 git config --global pager.show `brew --prefix`/share/git-core/contrib/diff-highlight/diff-highlight
