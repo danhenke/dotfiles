c9ea5ac8247608033056b988bda0114e28d81f9a
Dan Henke <dan@thinkalike.com>
2019-10-18 14:07:14 +1000

    Reverting some coreutils related changes

--- bin/zshrc.sh
+++ bin/zshrc.sh
@@ -5,14 +5,6 @@ set -e
 touch ~/.hushlogin
 
 cat << EOF > $HOME/.zshrc
-# Tell ls to be colourful
-export TERM="xterm-color"
-export CLICOLOR="1"
-export LSCOLORS="Exfxcxdxbxegedabagacad"
-
-# Tell grep to highlight matches
-export GREP_OPTIONS="--color=auto"
-
 # Pretty prompt
 export PROMPT="%F{cyan}%n%f@%F{green}%m:%f%B%F{yellow}%~%f%b %# "
 
@@ -21,17 +13,9 @@ export PATH="\$PATH:\$(go env GOPATH)/bin"
 export GOPROXY="direct"
 export GOSUMDB="off"
 
-# Brew
-BREW_PREFIX="\$(brew --prefix)"
-export PATH="\${BREW_PREFIX}/opt/coreutils/libexec/gnubin:\$PATH"
-export PATH="\${BREW_PREFIX}/opt/findutils/libexec/gnubin:\$PATH"
-export PATH="\${BREW_PREFIX}/opt/gnu-sed/libexec/gnubin:\$PATH"
-export PATH="\${BREW_PREFIX}/opt/grep/libexec/gnubin:\$PATH"
-export PATH="\${BREW_PREFIX}/opt/make/libexec/gnubin:\$PATH"
-
 # Aliases
-alias ls="ls -GFhl --color=auto"
-alias git="hub"
+alias ls="ls -GFhl"
+alias grep="grep --color=auto"
 EOF
 
 # Apply changes to current bash environment
