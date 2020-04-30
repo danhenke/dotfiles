79a4280675a935e93970c987c557ca04f309b18f
Dan Henke <dan@thinkalike.com>
2020-04-30 14:42:53 +1000

    Added git log alias

--- bin/git.sh
+++ bin/git.sh
@@ -7,6 +7,7 @@ git config --global credential.helper osxkeychain
 git config --global pager.log `brew --prefix`/share/git-core/contrib/diff-highlight/diff-highlight
 git config --global pager.show `brew --prefix`/share/git-core/contrib/diff-highlight/diff-highlight
 git config --global pager.diff `brew --prefix`/share/git-core/contrib/diff-highlight/diff-highlight
+git config --global alias.l "log --pretty='%C(dim cyan)%h%C(reset) %C(yellow)|%C(reset) %s%C(cyan)%d%C(reset) %C(dim)by %an %ar' --first-parent -20"
 git config --global url.git@github.com:.insteadOf https://github.com/
 git config --global core.excludesfile '~/.gitignore.global'
 git config --global push.followTags true
