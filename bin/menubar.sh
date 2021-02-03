43f9790b0214e1b866b3dbe8a3bf4c9bdd7dc2e8
Dan Henke <dan@thinkalike.com>
2021-02-04 02:01:43 +1000

    No longer needed

--- bin/menubar.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Show battery percentage remaining
-defaults write com.apple.menuextra.battery ShowPercent -string "YES"
-
-# Show date & AM/PM time in the clock
-defaults write com.apple.menuextra.clock DateFormat -string "EEE d MMM h:mm a"
