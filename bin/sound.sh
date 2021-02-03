0895897d57f3e599277818dae39f59bc567fa23c
Dan Henke <dan@thinkalike.com>
2021-02-04 02:29:36 +1000

    No longer needed

--- bin/sound.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Enable volume change feedback
-defaults write NSGlobalDomain com.apple.sound.beep.feedback -int 1
-
-# Increase sound quality for Bluetooth headphones/headsets
-defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
