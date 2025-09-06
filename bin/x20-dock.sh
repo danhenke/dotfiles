2c6b81549d4c5cbfa78b6fcab835042ae87b5efa
Dan Henke <dan@thinkalike.com>
2025-09-06 22:07:45 +1000

    dock additions

--- bin/x20-dock.sh
+++ bin/x20-dock.sh
@@ -1,8 +1,17 @@
 # Automatically hide and show the Dock
-defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock "autohide" -bool "true"
 
-# Dont show the dock for 1000 seconds
-defaults write com.apple.dock autohide-delay -float 1000
+# Dont show the dock for 1 seconds
+defaults write com.apple.dock "autohide-delay" -float "1"
+
+# Show only open applications in the Dock
+defaults write com.apple.dock static-only -bool true
+
+# Set the dock icon size
+defaults write com.apple.dock tilesize -int 64
+
+# Dont display recent apps in the Dock
+defaults write com.apple.dock "show-recents" -bool "false"
 
 # Restart the dock to apply changes
 killall Dock
