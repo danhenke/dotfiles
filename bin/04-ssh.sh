fcee3ff353116621c135d4dfe99c4ce9bba12d17
Dan Henke <dan@thinkalike.com>
2025-09-06 20:44:26 +1000

    make sure ssh keys added to agent

--- bin/04-ssh.sh
+++ bin/04-ssh.sh
@@ -9,3 +9,8 @@ AddKeysToAgent yes
 IdentityFile ~/Documents/ssh/id_ed25519
 EOF
 fi
+
+# If ssh keys are not already added, add them
+if ! ssh-add -l | grep -e 'ED25519'; then
+  ssh-add --apple-use-keychain ~/Documents/ssh/id_ed25519
+fi
