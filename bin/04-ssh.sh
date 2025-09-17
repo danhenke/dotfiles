720b8a3907bbaeb070e5706eebf02122cc805d94
Dan Henke <dan@thinkalike.com>
2025-09-17 14:27:08 +1000

    restructure

--- bin/04-ssh.sh
+++ bin/04-ssh.sh
@@ -1,16 +1,8 @@
+# Restore SSH config
 mkdir -p "$HOME/.ssh"
-
-# Regenerate SSH config
-if [ ! -e "$HOME/.ssh/config" ]; then
-cat << EOF > $HOME/.ssh/config
-Host *
-UseKeychain yes
-AddKeysToAgent yes
-IdentityFile ~/Documents/ssh/id_ed25519
-EOF
-fi
+cp "$DIR/ssh/config" "$HOME/.ssh/config"
 
 # If ssh keys are not already added, add them
-if ! ssh-add -l | grep -e 'ED25519'; then
-  ssh-add --apple-use-keychain ~/Documents/ssh/id_ed25519
+if ! ssh-add -l | grep -e "ED25519"; then
+  ssh-add --apple-use-keychain "$HOME/Documents/ssh/id_ed25519"
 fi
--- bin/07-starship.sh
+++ bin/07-starship.sh
@@ -1,35 +1,3 @@
+# Restore starship config
 mkdir -p "$HOME/.config"
-
-# Regenerate starship config
-if [ ! -e "$HOME/.config/starship.toml" ]; then
-cat << EOF > $HOME/.config/starship.toml
-# Refer https://starship.rs/config/
-# Get editor completions based on the config schema
-"$schema" = 'https://starship.rs/config-schema.json'
-
-add_newline = false
-
-[hostname]
-ssh_only = false
-style = 'green'
-format = '[$ssh_symbol$hostname]($style)(white):'
-
-[cmd_duration]
-disabled = true
-
-[directory]
-truncation_length = 5
-truncation_symbol = '…/'
-style = 'bold yellow'
-
-[line_break]
-disabled = true
-
-[username]
-style_user = 'cyan'
-style_root = 'black bold'
-format = '[$user]($style)@'
-disabled = false
-show_always = true
-EOF
-fi
+cp "$DIR/starship/starship.toml" "$HOME/.config/starship.toml"
