d417ddf648245edd0d5b68be46f81e16ce00bc36
Dan Henke <contact@danhenke.au>
2025-11-30 11:51:20 +1000

    Improved github ssh key setup

--- bin/04-ssh.sh
+++ bin/04-ssh.sh
@@ -2,7 +2,7 @@
 mkdir -p "$HOME/.ssh"
 cp "$DIR/ssh/config" "$HOME/.ssh/config"
 
-# If ssh keys are not already added, add them
-if ! ssh-add -l | grep -e "ED25519"; then
-  ssh-add --apple-use-keychain "$HOME/Documents/ssh/id_ed25519"
+# Ensure SSH keys are loaded in SSH agent
+if ! ssh-add -L | grep -qe "GitHub$"; then
+  ssh-add --apple-use-keychain "$HOME/Documents/ssh/github_id_ed25519"
 fi
--- bin/05-git.sh
+++ bin/05-git.sh
@@ -3,9 +3,9 @@ cp "$DIR/git/gitconfig" "$HOME/.gitconfig"
 cp "$DIR/git/gitignore" "$HOME/.gitignore"
 
 # Restore git user config
-git config --global user.email "$(git log -1 --pretty=format:'%ae')"
 git config --global user.name "$(git log -1 --pretty=format:'%an')"
-git config --global user.signingkey "$(ssh-add -L)"
+git config --global user.email "$(git log -1 --pretty=format:'%ae')"
+git config --global user.signingkey "$(ssh-add -L | grep -e "GitHub$")"
 
 # Authenticate github cli
-gh auth login --web --skip-ssh-key --hostname github.com --git-protocol ssh
+gh auth login --web --skip-ssh-key --git-protocol ssh
