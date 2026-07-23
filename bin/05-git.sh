c423297b6ad6d1139270fdf2ca4d056488b66da9
Dan Henke <contact@danhenke.au>
2026-07-23 16:52:21 +1000

    feat(git): configure SSH commit signature verification

--- bin/05-git.sh
+++ bin/05-git.sh
@@ -8,4 +8,11 @@ git config --global user.email "$(git log -1 --pretty=format:'%ae')"
 git config --global user.signingkey "$(ssh-add -L | grep -e "GitHub$")"
 
 # Authenticate github cli
-gh auth login --web --skip-ssh-key --git-protocol ssh
+gh auth login --web --skip-ssh-key --git-protocol ssh --scopes user
+
+# Setup allowed signers
+git config --global gpg.ssh.allowedSignersFile "~/.ssh/allowed_signers"
+gh api user/emails --jq '.[] | select(.verified == true) | .email' |
+while IFS= read -r email; do
+  printf '%s %s\n' "$email" "$(git config user.signingkey)"
+done >> ~/.ssh/allowed_signers
